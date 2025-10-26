7aff80dbad4f7d1bcd5563e7145cdea72b37e004
elena <elenajeanday@gmail.com>
2025-10-26 14:19:25 +0000

    adding changes to mimic new paths

--- main.jsx
+++ main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import { GetSessionId, SetSessionId } from "./functionality/session-storage";
-import { HeaderBar, FooterBar } from "./components/header-footer-bar";
-import { WebpageRoutes } from "./functionality/routes";
-import "./css/core.module.css";
+import { GetSessionId, SetSessionId } from "./client/src/functionality/session-storage";
+import { HeaderBar, FooterBar } from "./client/src/components/header-footer-bar";
+import { WebpageRoutes } from "./client/src/functionality/routes";
+import "./client/src/css/core.module.css";
 
 if(!GetSessionId()){
   SetSessionId()
